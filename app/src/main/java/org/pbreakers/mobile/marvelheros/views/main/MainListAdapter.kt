6bfe87d97600103fa8ae011c0a80acaf25cdab98
Eric Ampire <ericampire.top@gmail.com>
2019-04-27 21:55:21 +0200

    [ADD] MainListAdapter.kt

---
+++ app/src/main/java/org/pbreakers/mobile/marvelheros/views/main/MainListAdapter.kt
@@ -0,0 +1,6 @@
+package org.pbreakers.mobile.marvelheros.views.main
+
+import org.pbreakers.mobile.marvelheros.views.common.AnyItemAdapter
+import org.pbreakers.mobile.marvelheros.views.common.RecyclerListAdapter
+
+class MainListAdapter(val data: List<AnyItemAdapter>) : RecyclerListAdapter(data)
