11af33d0f6b5b47b454c0e5791ce4acd784db2f0
Eric Ampire <ericampire.top@gmail.com>
2019-04-28 06:39:33 +0200

    [UPDATE] We can now add or delete one item

--- app/src/main/java/org/pbreakers/mobile/marvelheros/views/main/MainListAdapter.kt
+++ app/src/main/java/org/pbreakers/mobile/marvelheros/views/main/MainListAdapter.kt
@@ -3,4 +3,20 @@ package org.pbreakers.mobile.marvelheros.views.main
 import org.pbreakers.mobile.marvelheros.views.common.AnyItemAdapter
 import org.pbreakers.mobile.marvelheros.views.common.RecyclerListAdapter
 
-class MainListAdapter(val data: List<AnyItemAdapter>) : RecyclerListAdapter(data)
+class MainListAdapter(var data: List<AnyItemAdapter>) : RecyclerListAdapter(data) {
+    fun add(item: AnyItemAdapter) {
+        data += item
+        val index = data.indexOf(item)
+
+        if (index == -1) return
+        notifyItemInserted(index)
+    }
+
+    fun delete(item: AnyItemAdapter) {
+        val index = data.indexOf(item)
+
+        if (index == -1) return
+        data -= item
+        notifyItemRemoved(index)
+    }
+}
